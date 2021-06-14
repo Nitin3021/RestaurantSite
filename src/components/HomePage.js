f66748446ae65b8c4f5de0f947f2feabad0c7459
Nitin <nitinpillai30@yahoo.com>
2021-06-14 21:01:49 +0700

    Adding & styling items with text

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -7,28 +7,47 @@ const HomePage = () => (
 
         <div className="square">
             <img src="/images/Food_item_1.png" />
-            <p className="menu-txt">Pancit is a Filipino dish that usually consists of noodles cooked with soy sauce </p>
+            <p>Pancit is a staple Filipino dish found at numerous feasts and celebrations, consisting of stir-fried noodles with meat and vegetables such as chicken, pork, shrimp, celery, carrots, onions, garlic, and cabbage. The dish has Chinese origins, and the name of the dish is derived from the Hokkien phrase pian i sit, meaning something convenient, cooked fast.</p>
         </div>
 
         <div className="square">
-            <div className="square img_1-2">
-            </div>
-            <div className="square img_1-3">
-            </div>
+            <img src="/images/Food_item_2.jfif" />
+            <p>Sinigang is a Filipino soup or stew characterized by its sour and savoury taste. It is most often associated with tamarind (Filipino: sampalok), although it can use other sour fruits and leaves as the souring agent. It is one of the more popular dishes in Filipino cuisine.</p>
+        </div>
 
-            <div className="square img_2-1">
-            </div>
-            <div className="square img_2-2">
-            </div>
-            <div className="square img_2-3">
-            </div>
+        <div className="square">
+            <img src="/images/Food_item_3.jpg" />
+            <p>Adobo refers to a method of marinating and stewing for any cut of meat or fish in a briny mixture of vinegar, soy sauce, and spices. Filipino adobo should not be confused with the spicy Spanish adobo sauce.</p>
+        </div>
 
-            <div className="square img_3-1">
-            </div>
-            <div className="square img_3-2">
-            </div>
-            <div className="square img_3-3">
-            </div>
+        <div className="square">
+            <img src="/images/Food_item_4.jfif" />
+            <p>The hand-breaded fried chicken is praised for being crispy and juicy; it's made with a secret marinade and served with a side of gravy that's been called “ethereal.” It is often ordered as a combo with Jolly Spaghetti.</p>
+        </div>
+
+        <div className="square">
+            <img src="/images/Food_item_5.jfif" />
+            <p>Pancit is a Filipino dish that usually consists of noodles cooked with soy sauce </p>
+        </div>
+
+        <div className="square">
+            <img src="/images/Food_item_6.jfif" />
+            <p>Pancit is a Filipino dish that usually consists of noodles cooked with soy sauce </p>
+        </div>
+
+        <div className="square">
+            <img src="/images/Food_item_7.jpg" />
+            <p>Pancit is a Filipino dish that usually consists of noodles cooked with soy sauce </p>
+        </div>
+
+        <div className="square">
+            <img src="/images/Food_item_8.jpg" />
+            <p>Pancit is a Filipino dish that usually consists of noodles cooked with soy sauce </p>
+        </div>
+
+        <div className="square">
+            <img src="/images/Food_item_9.jpg" />
+            <p>Pancit is a Filipino dish that usually consists of noodles cooked with soy sauce </p>
         </div>
     </div>
 );
