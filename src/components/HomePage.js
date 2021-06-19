2feafaeb26fcdce313cf498132b3b893e8eb007f
Nitin <nitinpillai30@yahoo.com>
2021-06-19 18:07:52 +0700

    Refactoring HomePage & MenuPage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,57 +1,89 @@
 import React from 'react';
+import { Fade } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css';
 
-const HomePage = () => (
-    <div className="box-layout">
-        <div className="text-homepage-header">
-            <h1>Welcome</h1>
-            <p>Take a look around!</p>
-        </div>
+const fadeImages = [
+    '/images/Food_item_1.jpg',
+    '/images/Food_item_2.jpg',
+    '/images/Food_item_3.jpg',
+    '/images/Food_item_4.jpg',
+    '/images/Food_item_5.jpg',
+    '/images/Food_item_6.jpg',
+    '/images/Food_item_7.jpg',
+    '/images/Food_item_8.jpg',
+    '/images/Food_item_9.jpg'
+];
 
-        <div className="square">
-            <img src="/images/Food_item_1.jpg" />
-            <p>Pancit is a staple Filipino dish found at numerous feasts and celebrations, consisting of stir-fried noodles with meat and vegetables such as chicken, pork, shrimp, celery, carrots, onions, garlic, and cabbage. The dish has Chinese origins, and the name of the dish is derived from the Hokkien phrase pian i sit, meaning something convenient, cooked fast.</p>
-        </div>
+const HomePage = () => {
+    return (
+        <div className="box-layout">
 
-        <div className="square">
-            <img src="/images/Food_item_2.jpg" />
-            <p>Sinigang is a Filipino soup or stew characterized by its sour and savoury taste. It is most often associated with tamarind (Filipino: sampalok), although it can use other sour fruits and leaves as the souring agent. It is one of the more popular dishes in Filipino cuisine.</p>
-        </div>
+            <div className="text-homepage-header">
+                <h1>Welcome</h1>
+                <p>Take a look around!</p>
+            </div>
 
-        <div className="square">
-            <img src="/images/Food_item_3.jpg" />
-            <p>Adobo refers to a method of marinating and stewing for any cut of meat or fish in a briny mixture of vinegar, soy sauce, and spices. Filipino adobo should not be confused with the spicy Spanish adobo sauce.</p>
-        </div>
+            <div className="slide-container">
+                <Fade>
+                    <div className="each-fade">
+                        <h2 className="slide-text">Pancit</h2>
+                        <div className="image-container">
+                            <img className="slide-style" src={fadeImages[0]} />
+                        </div>
+                    </div>
+                    <div className="each-fade">
+                        <h2 className="slide-text">Sinigang</h2>
+                        <div className="image-container">
+                            <img className="slide-style" src={fadeImages[1]} />
+                        </div>
+                    </div>
+                    <div className="each-fade">
+                        <h2 className="slide-text">Adobo</h2>
+                        <div className="image-container">
+                            <img className="slide-style" src={fadeImages[2]} />
+                        </div>
+                    </div>
+                    <div className="each-fade">
+                        <h2 className="slide-text">Special Chicken!</h2>
+                        <div className="image-container">
+                            <img className="slide-style" src={fadeImages[3]} />
+                        </div>
+                    </div>
+                    <div className="each-fade">
+                        <h2 className="slide-text">Halo Halo!</h2>
+                        <div className="image-container">
+                            <img className="slide-style" src={fadeImages[4]} />
+                        </div>
+                    </div>
+                    <div className="each-fade">
+                        <h2 className="slide-text">Spageee</h2>
+                        <div className="image-container">
+                            <img className="slide-style" src={fadeImages[5]} />
+                        </div>
+                    </div>
+                    <div className="each-fade">
+                        <h2 className="slide-text">Pizza</h2>
+                        <div className="image-container">
+                            <img className="slide-style" src={fadeImages[6]} />
+                        </div>
+                    </div>
+                    <div className="each-fade">
+                        <h2 className="slide-text">Lasagna</h2>
+                        <div className="image-container">
+                            <img className="slide-style" src={fadeImages[7]} />
+                        </div>
+                    </div>
+                    <div className="each-fade">
+                        <h2 className="slide-text">Buffet!</h2>
+                        <div className="image-container">
+                            <img className="slide-style" src={fadeImages[8]} />
+                        </div>
+                    </div>
+                </Fade>
+            </div>
 
-        <div className="square">
-            <img src="/images/Food_item_4.jpg" />
-            <p>The hand-breaded fried chicken is praised for being crispy and juicy; it's made with a secret marinade and served with a side of gravy that's been called “ethereal.” It is often ordered as a combo with Jolly Spaghetti.</p>
         </div>
-
-        <div className="square">
-            <img src="/images/Food_item_5.jpg" />
-            <p>Halo-halo, also spelled haluhalo, Filipino for "mixed", is a popular cold dessert in the Philippines made up of crushed ice, evaporated milk or condensed milk, and various ingredients including: ube, sweetened beans, coconut strips, sago, gulaman (agar), pinipig rice, boiled taro or soft yams in cubes, slices or portions of fruit preserves and other root crop preserves, flan, and often topped with a scoop of ube ice cream.</p>
-        </div>
-
-        <div className="square">
-            <img src="/images/Food_item_6.jpg" />
-            <p>Filipino Spaghetti is the Pinoy version of Spaghetti with meat sauce. This version has a sweet tomato based sauce with lots of meat ingredients such as ground pork, luncheon meat, and hotdogs.</p>
-        </div>
-
-        <div className="square">
-            <img src="/images/Food_item_7.jpg" />
-            <p>Pizza bianca, or 'white pizza', is nothing more than a thinly baked dough with olive oil and sea salt.</p>
-        </div>
-
-        <div className="square">
-            <img src="/images/Food_item_8.jpg" />
-            <p>Filipino Style Lasagna is a sweet, cheesy and creamy pasta. It is made with saucy ground beef meat sauce, layered with a creamy bechamel sauce and finally topped with lots of mozzarella cheese.</p>
-        </div>
-
-        <div className="square">
-            <img src="/images/Food_item_9.jpg" />
-            <p>Dishes range from the very simple, like a meal of fried salted fish and rice, to fish curry, chicken curry, complex paellas and cozidos of Iberian origin created for fiestas.</p>
-        </div>
-    </div>
-);
+    )
+};
 
 export default HomePage;
