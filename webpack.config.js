8e02bdc05c6d3a22231fc322429fd63d2128b537
Nitin <nitinpillai30@yahoo.com>
2021-07-01 21:28:21 +0700

    Tidying up webpack.config, adding styles to header component

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,3 @@
-// const path = require('path');
-
-// module.exports = {
-//     entry: './src/app.js',
-//     output: {
-//         path: path.join(__dirname, 'public'),
-//         filename: 'bundle.js'
-//     },
-//     module: {
-//         rules: [{
-//             loader: 'babel-loader',
-//             test: /\.js$/,
-//             exclude: /node_modules/
-//         }, {
-//             test: /\.s?css$/,
-//             use: [
-//                 'style-loader',
-//                 'css-loader',
-//                 'sass-loader'
-//             ]
-//         }]
-//     },
-//     devServer: {
-//         contentBase: path.join(__dirname, 'public'),
-//         historyApiFallback: true
-//     }
-// };
-
 const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
